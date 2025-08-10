e4ba748f311da0f67eb12982c32d69d1aa776e2a
Siddhant Kalagate <siddhantkalagate01@gmail.com>
2025-08-10 22:53:05 +0530

    task2.py
    This program focuses on string manipulation and user interaction in Python.
    It asks the user to enter their first name and last name, then combines them
    into a full name to display a personalized greeting.

---
+++ task2.py
@@ -0,0 +1,6 @@
+# taking inputs from the user
+first_name = (input("enter your first name: "))
+last_name = (input("enter your last name: "))
+
+#Printing personalized greeting 
+print(f"hello, {first_name} {last_name}! welcome to the python program")
